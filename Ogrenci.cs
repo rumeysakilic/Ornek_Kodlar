5539bd549ce9707136869cda98dc6867e6aedc65
Rumeysa KILIÇ <111907567+rumeysakilic@users.noreply.github.com>
2023-07-13 14:13:04 +0300

    Öğrenci Uygulaması
    Class yapılarını kullanarak öğrenci bilgilerinin tutulduğu program.

---
+++ Ogrenci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciUygulamasi
+{
+    internal class Ogrenci
+    {
+        private string ogr_no;
+        private string isim;
+        private string soyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okulIsmi;
+
+        public Ogrenci(string _ogrNo,string _isim,string _soyisim,int _vize1,int _vize2, int _final,string _okulIsmi)
+        {
+            ogr_no = _ogrNo;
+            isim = _isim;
+            soyisim = _soyisim;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+            okulIsmi = _okulIsmi;
+        }
+
+        public void ogrenciBilgileriGoster()
+        {
+            Console.WriteLine("Öğrenci No:" + ogr_no);
+            Console.WriteLine("Öğrenci Adı:" + isim);
+            Console.WriteLine("Öğrenci Soyadı:" + soyisim);
+            Console.WriteLine("Öğrenci 1.vize notu:" + vize1);
+            Console.WriteLine("Öğrenci 2. vize notu:" + vize2);
+            Console.WriteLine("Öğrenci final  notu:" + final);
+            Console.WriteLine("Öğrenci okul ismi:" + okulIsmi);
+        }
+        public void ortalamaBul()
+        {
+            double ort;
+            ort = (vize1 + vize2 + final) / 3;
+            Console.WriteLine(isim + " isimli öğrencinin not ortalaması: " + ort);
+        }
+        public void okulGetir()
+        {
+            Console.WriteLine("Öğrencinin okul ismi:" +okulIsmi);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Ogrenci ogr1= new Ogrenci("201213002","Rumeysa","KILIÇ",75,75,85,"Konya Teknik Üniversitesi");
+            Console.WriteLine("Öğrenci Bilgi Ekranına Hoş Geldiniz! Lütfen bir seçim yapınız!");
+            Console.WriteLine("1- Öğrenci Bilgi Göster");
+            Console.WriteLine("2-Öğrenci Ortalamasını Göster");
+            Console.WriteLine("3-Öğrenci Okulunu Öğren");
+            Console.WriteLine("4-Çıkış Yap");
+            int secim = Convert.ToInt32(Console.ReadLine());
+            if(secim == 1)
+            {
+                ogr1.ogrenciBilgileriGoster();
+            }
+            else if(secim == 2)
+            {
+                ogr1.ortalamaBul();
+            }
+            else if(secim == 3)
+            {
+                ogr1.okulGetir();
+            }
+            else if(secim == 4) 
+            {
+                Console.WriteLine("Çıkış yapılıyor...");
+            }
+
+        }
+    }
+}
